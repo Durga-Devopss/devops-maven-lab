774654f1c4ab3bf2a5eea9822b2002b353d60ec2
durga <durgagoli163@gmail.com>
2026-09-07 04:37:40 +0000

    Update Maven lab application message

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -2,6 +2,6 @@ package com.devops;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("DevOps Maven Lab is working!");
+        System.out.println("DevOps Maven Lab v2 is working!");
     }
 }
